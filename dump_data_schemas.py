59e35221b7d034498d4bb43087437cdfedc3707f
Josh McDaniel <80354972+joshrmcdaniel@users.noreply.github.com>
2022-11-26 15:00:51 -0600

    support for filetypes (not tested)

--- dump_data_schemas.py
+++ dump_data_schemas.py
@@ -5,8 +5,24 @@ import os
 from argparse import ArgumentParser
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import freeze_support
+from rarfile import RarFile
 from tqdm.contrib.concurrent import process_map
+from tarfile import TarFile
+from typing import Dict, List, TypeVar, Union
 from zipfile import ZipFile
+
+
+Compressed = TypeVar('Compressed', RarFile, ZipFile, TarFile)
+
+
+MIME_MAP: Dict[str, Compressed] = {
+    'application/zip': ZipFile,
+    'application/vnd.rar': RarFile,
+    'application/x-tar': 'tar',
+    'application/gzip': 'gz',
+    'application/x-bzip': 'bz',
+    'application/x-xz': 'xz'
+}
 
 
 DATA_PATH = None
@@ -34,31 +50,59 @@ def get_schemas(file: str) -> None:
     """
     file_path = os.path.join(DATA_PATH, file)
     #  TODO: add magic header handling
-    mime = magic.from_file(file_path)
-    with ZipFile(DATA_PATH+file, 'r') as f:
-        with ThreadPoolExecutor(max_workers=THREAD_POOL_COUNT) as proc_pool:
-            for f_name in f.namelist():
-                proc_pool.submit(get_schema_from_zip, f_name, f)
+    mime = magic.from_file(file_path, mime=True)
+    file_obj = MIME_MAP.get(mime, None)
+    if file_obj:
+        if isinstance(file_obj, str):
+            #  Assumes tar file based on mapping
+            if ext == 'tar':
+                ext = ''
+            file_obj = lambda x, m: TarFile(x, f'{m}:{ext}')
+        with file_obj(DATA_PATH+file, 'r') as f:
+            with ThreadPoolExecutor(max_workers=THREAD_POOL_COUNT) as proc_pool:
+                f_names = get_filenames(f)
+                for f_name in f_names:
+                    proc_pool.submit(get_schema_from_file, f_name, f)
+    elif mime == 'text/plain':
+        get_schema_from_file(file)
+    else:
+        raise NotImplementedError(f'no support for {mime}')
 
 
-def get_schema_from_zip(file_name: str, fp: ZipFile) -> None:
+def get_schema_from_file(file_name: str, fp: Union[Compressed, None]=None) -> None:
     """
     Get lines of file content in a ZIP file
     """
     try:
-        handle = fp.open(file_name, 'r')
-        lines = b''.join(handle.readline() for _ in range(SCHEMA_LINES))
+        if fp:
+            if isinstance(fp, TarFile):
+                handle = fp.extractfile(file_name)
+            else:
+                handle = fp.open(file_name, 'r')
+        else:
+            handle = open(file_name, 'rb')
+        handle.seek(-1,2)
+        eof = handle.tell()
+        handle.seek(0,0)
+        lines = b''.join(handle.readline() for _ in range(SCHEMA_LINES) if handle.tell() != eof)
     except (FileNotFoundError, IOError) as fe:
         if isinstance(fe, FileNotFoundError):
             print(f'{file_name} does not exist in {fp.filename}')
         else:
-            print('No lines to read')
+            print('IOError')
         print(f'Exception info: {fe}')
     else:
         with open(f'{SCHEMA_PATH}{file_name}.schema', 'wb') as wf:
             wf.write(lines)
     finally:
         handle.close()
+
+
+def get_filenames(f: Union[RarFile, TarFile, ZipFile]) -> List[str]:
+    if isinstance(f, (RarFile, ZipFile)):
+        return f.namelist()
+    elif isinstance(f, TarFile):
+        return f.getnames()
 
 
 def set_globes(data_path: str, schema_path: str, proc_count: int, tp_count: int, lines: int):
